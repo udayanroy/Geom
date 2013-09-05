000b6512d212576eca02f2b62d54eb270b7217d0
udayan <udaroy@rediffmail.com>
2013-09-05 07:56:52 +0530

    GMatrix class added

---
+++ src/GMatrix.vb
@@ -0,0 +1,118 @@
+﻿Namespace Geometry
+
+
+
+    Public Class GMatrix
+
+        Const deg2rad As Double = 0.017453292519943295
+
+        Dim _dx, _dy, _m11, _m12, _m21, _m22 As Single
+
+
+        Public Sub New()
+
+        End Sub
+
+        Public Function isInvertible() As Boolean
+            Return ((_m11 * _m22 - _m12 * _m21) < 0.00001)
+        End Function
+
+        Public Function determinant() As Single
+            Return _m11 * _m22 - _m12 * _m21
+        End Function
+
+        Public Function Invert() As Boolean
+            Dim dtr = determinant()
+            If dtr = 0.0 Then
+                Return False
+            Else
+                Dim dinv = 1.0 / dtr
+
+                Dim mat(6) As Single
+                mat(0) = _m22 * dinv
+                mat(1) = -_m12 * dinv
+                mat(2) = -_m21 * dinv
+                mat(3) = _m11 * dinv
+                mat(4) = (_m21 * _dy - _m22 * _dx) * dinv
+                mat(5) = (_m12 * _dx - _m11 * _dy) * dinv
+
+                _m11 = mat(0)
+                _m12 = mat(1)
+                _m21 = mat(2)
+                _m22 = mat(3)
+                _dx = mat(4)
+                _dy = mat(5)
+
+                Return True
+            End If
+        End Function
+        Public Sub reset()
+            _dx = 0.0F
+            _dy = 0.0F
+            _m12 = 0.0F
+            _m21 = 0.0F
+
+            _m11 = 1.0F
+            _m22 = 1.0F
+        End Sub
+
+        Public Sub Translate(ByVal dx As Single, ByVal dy As Single)
+            _dx += dx * _m11 + dy * _m21
+            _dy += dy * _m22 + dx * _m12
+        End Sub
+
+        Public Sub Scale(ByVal sx As Single, ByVal sy As Single)
+            _m11 *= sx
+            _m12 *= sx
+            _m21 *= sy
+            _m22 *= sy
+        End Sub
+        Public Sub Roatate(ByVal a As Single)
+            Dim sina As Single = 0
+            Dim cosa As Single = 0
+            If (a = 90 Or a = -270) Then
+                sina = 1
+            ElseIf (a = 270 Or a = -90) Then
+                sina = -1
+            ElseIf (a = 180) Then
+                cosa = -1
+            Else
+                Dim b = deg2rad * a
+                sina = Math.Sin(b)
+                cosa = Math.Cos(b)
+            End If
+            Dim tm11 = cosa * _m11 + sina * _m21
+            Dim tm12 = cosa * _m12 + sina * _m22
+            Dim tm21 = -sina * _m11 + cosa * _m21
+            Dim tm22 = -sina * _m12 + cosa * _m22
+            _m11 = tm11
+            _m12 = tm12
+            _m21 = tm21
+            _m22 = tm22
+        End Sub
+        Public Sub Shear(ByVal sh As Single, ByVal sv As Single)
+            Dim tm11 = sv * _m21
+            Dim tm12 = sv * _m22
+            Dim tm21 = sh * _m11
+            Dim tm22 = sh * _m12
+            _m11 += tm11
+            _m12 += tm12
+            _m21 += tm21
+            _m22 += tm22
+        End Sub
+
+        Public Sub Transform(ByRef p As GPoint)
+            Dim fx = _m11 * p.X + _m21 * p.Y + _dx
+            Dim fy = _m12 * p.X + _m22 * p.Y + _dy
+            p.X = fx
+            p.Y = fy
+        End Sub
+        Public Sub Transform(ByRef points As GPoint())
+            For Each p As GPoint In points
+                Me.Transform(p)
+            Next
+        End Sub
+
+
+    End Class
+End Namespace
